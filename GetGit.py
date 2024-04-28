a68e1f48b0d7fbaf67f3f18ac3870966c54b1b86
Limplom <113057438+Limplom@users.noreply.github.com>
2024-04-28 21:49:54 +0200

    Update GetGit.py
    Renamed the function newest_releses to get_newest_releases to make the name more descriptive.
    
    Created the list of downloadable files directly using a list comprehension.
    
    Used Exception in the try-except clause more generally to catch different types of exceptions.
    
    Formatted the output of downloadable files in a numbered list for better readability.
    
    Extracted the file name using split('/')[-1] instead of relying on the count of "/" for better robustness, regardless of the length of the file path.

--- GetGit.py
+++ GetGit.py
@@ -1,21 +1,19 @@
-import requests, os, urllib.request
+import requests
 from bs4 import BeautifulSoup
+import os
+import urllib.request
 
-def newest_releses(URL):
+def get_newest_releases(url):
     try:
-        get_tags = requests.get(f'{URL}/tags/')
-        version = BeautifulSoup(get_tags.content, 'html.parser').find('a', attrs={'class':'Link--primary'}).get('href')
+        get_tags = requests.get(f'{url}/tags/')
+        version = BeautifulSoup(get_tags.content, 'html.parser').find('a', attrs={'class': 'Link--primary'}).get('href')
         new_url = f'https://github.com{version}'
-        download_page = requests.get(new_url.replace('tag','expanded_assets'))
-        find_downloads = BeautifulSoup(download_page.content,'html.parser').find_all('a',attrs={"data-view-component":"true","class":"Truncate"})
-        file_list = []
-        for href in find_downloads:
-            download = href['href']
-            file_list.append(f'https://github.com{download}')
+        download_page = requests.get(new_url.replace('tag', 'expanded_assets'))
+        find_downloads = BeautifulSoup(download_page.content, 'html.parser').find_all('a', attrs={"data-view-component": "true", "class": "Truncate"})
+        file_list = [f'https://github.com{href["href"]}' for href in find_downloads]
         return file_list
-    except:
-        return f'There aren’t any releases at {URL}'
-        
+    except Exception as e:
+        return f'There aren’t any releases at {url}'
 
 if __name__ == '__main__':
     os.system('cls')
@@ -35,17 +33,16 @@ if __name__ == '__main__':
     ''')
     git_url = input('URL from the Git : ')
     print('\n')
-    count = 1
-    get_return = newest_releses(git_url)
 
-    if isinstance(get_return, str) == True:
+    get_return = get_newest_releases(git_url)
+
+    if isinstance(get_return, str):
         print(get_return)
         input()
     else:
-        for i in get_return:
-            print(f'{count}.  {i}')
-            count+=1
+        for idx, file_url in enumerate(get_return, start=1):
+            print(f'{idx}. {file_url}')
         chosse = int(input('\nWhich one should be downloaded : '))
-        get_file_name = str(get_return[chosse-1]).split('/')
-        urllib.request.urlretrieve(get_return[chosse-1],get_file_name[len(str(get_return[chosse-1]).split('/'))-1])
+        file_name = get_return[chosse - 1].split('/')[-1]
+        urllib.request.urlretrieve(get_return[chosse - 1], file_name)
         input('\nDone\n')
